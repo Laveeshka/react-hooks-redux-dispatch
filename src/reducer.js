3148883ea6b4ffcc6f9997f83bb375d5113be5fb
LaveeshkaMahadea <laveeshka94@gmail.com>
2023-01-12 08:18:41 +1100

    render updates based on state change

--- src/reducer.js
+++ src/reducer.js
@@ -8,6 +8,18 @@ function changeState(state, action) {
 }
 
 let state = { count: 0 };
-let action = { type: "counter/increment" };
+//let action = { type: "counter/increment" };
 
-changeState(state, action);
+//changeState(state, action);
+
+function render() {
+  const app = document.querySelector("#app")
+  app.textContent = state.count
+}
+
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+
+render()
